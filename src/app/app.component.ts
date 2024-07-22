953b6fd5a2a755db0fdeafd396ae22a9fa31daa1
hicham maaqoul <hicham.maaqoul@gmail.com>
2024-07-22 10:25:03 +0100

    first try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-timepicker-demo';
+  startHour = '08';
+  startMinute = '00';
+  durationHour = '00';
+  durationMinute = '15';
+  showStartTimePicker = false;
+  showDurationPicker = false;
+
+  hours = this.generateHours();
+  minutes = this.generateMinutes();
+
+  generateHours(): string[] {
+    const hours: string[] = [];
+    for (let hour = 0; hour < 24; hour++) {
+      hours.push(this.padZero(hour));
+    }
+    return hours;
+  }
+
+  generateMinutes(): string[] {
+    return ['00', '15', '30', '45'];
+  }
+
+  padZero(num: number): string {
+    return num < 10 ? '0' + num : num.toString();
+  }
+
+  onStartTimeChange() {
+    this.showStartTimePicker = false;
+  }
+
+  onDurationChange() {
+    this.showDurationPicker = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatIconModule,
+    NgxMaterialTimepickerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
